b957c558e75d4538acec4673228169ffd196646b
Elaine Gao <egao1@umd.edu>
2024-11-26 17:27:43 -0500

    chucks data into a csv with columns name, championship, and time

--- src/main.py
+++ src/main.py
@@ -13,6 +13,8 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.action_chains import ActionChains
 
+extracted_data = []
+
 def getTeamID(team_name):
 	team_number = -1
 
@@ -22,7 +24,7 @@ def getTeamID(team_name):
 			team_number = row['team_ID']
 	return team_number
 
-def getWebpage(url):
+def getWebpage(url, extracted_data):
 	#set driver options
 	chrome_options = Options()
 	chrome_options.add_argument("--headless")
@@ -40,17 +42,32 @@ def getWebpage(url):
 		if table:
 			teams_list = table.find('tbody').find_all('tr')
 			print("Teams list found!")
-			# Access the fourth <tr> (index starts at 0, so the fourth is index 3)
-			fourth_tr = teams_list[3]
+			
+			if len(teams_list) < 4: 
+				# deals with if the 4th time isn't found,
+				# feel free to delete if you'd rather just not include it.
+				# if you want to leave it in, maybe you can move the championship
+				# field out of this, since it's retrieved again below :3
+				print("Fourth tr does not exist!")
+				name = "N/A" 
+				championship = teams_list[0].find_all('td')[2].find('a').text.strip()
+				time = "N/A"
 
-			# Print or process the fourth <tr>
-			print(fourth_tr)
+			else: 
+				fourth_tr = teams_list[3] # Access the fourth <tr> (index starts at 0, so the fourth is index 3)
+				# below works for my block-y output, since it's matching on the a hrefs w/in the HTML
+				name = fourth_tr.find_all('td')[1].find('a').text.strip() 
+				championship = fourth_tr.find_all('td')[2].find('a').text.strip()
+				time = fourth_tr.find_all('td')[3].find('a').text.strip()
+				
+			extracted_data.append([name, championship, time])
+			#print(extracted_data)
 		else:
 			print("Table with class 'c-table-clean' not found.")
 	except AttributeError:
 		print("Error: The table or tbody element is missing.")
-
-
+	
+	driver.quit()
 
 teams = pd.read_csv('collegeSwimmingTeams.csv')
 
@@ -75,12 +92,25 @@ events = ['150', '1100', '1200', '1500', '11000', '11650',
 
 temp_events = ['150']
 
+count = 0 # remove this
 
 for value in teams.iloc[:, 0]:  # .iloc[:, 0] selects the first column
 	team = value
 	curr_id = getTeamID(value)
 	for event in temp_events:
 		url = 'https://www.swimcloud.com/team/'+ str(curr_id) + '/times/?dont_group=false&event='+ event + '&event_course=Y&gender=M&page=1&region&season_id=27&tag_id&team_id=' + str(curr_id) + '&year=2024'
-		getWebpage(url)
+		getWebpage(url, extracted_data)
+
+	count += 1
+	if count == 11: # only going up to 10 for the sake of testing. otherwise, very slow...
+		break
+
+# writes out to csv after the loop has finished
+with open('fourth_best_time_results.csv', 'w', newline='', encoding='utf-8') as csvfile:
+		writer = csv.writer(csvfile)
+		writer.writerow(['Name', 'Championship', 'Time'])
+		writer.writerows(extracted_data)
+
+		print("Data has been written to fourth_best_time_results.csv")
 		
 
